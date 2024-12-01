c7c3b288312cd96aefdd6e6cbe7d5b04dcbe4b47
vineeth05codes <vineeththungani@gmail.com>
2024-12-01 23:21:55 +0530

    ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,17 @@
+import Conversation from "./Conversation"
+
+const Conversations = () => {
+  return (
+    <div>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+    </div>
+  )
+}
+
+export default Conversations
